2b72e604ced6b4abf9e0053a00fb7e6b874303d9
AnthonyJTaylor <33767381+AnthonyJTaylor@users.noreply.github.com>
2017-11-30 15:49:59 -0800

    added event mangment

--- paperclone/main.cpp
+++ paperclone/main.cpp
@@ -4,31 +4,33 @@
 int main(void) {
 	srand(time(NULL));// THIS LINE MUST EXECUTE FIRST AND ONLY ONCE 
 	gamedata data;
-
+	sf::Event event;
+	sf::Keyboard keyboard
 	//Okay, we're doing the game logic in  main
 	// We'll seee how long I keep that up
 	//gameplay loop runs until playerfinishes the month or loses all lives;
 	//Once events are figgered out, be sure that the player can exit at their discretion 
-	for (char input; data.getlives() > 0 && data.getday() < 12;) {
+	while(data.getlives() > 0 && data.getday() < 12;) {
 		data.genchara();
 		//placeholder for actually displaying to the screen
 		data.debugshowdata();
 		//get user input for judging the documents
 		//replace with events once that's figured
 		//for now a basic input loop
-		do {
-			std::cout << "Z:Documents are Invalid" << std::endl << " X:Documents are Genuine" << std::endl;
-			std::cin >> input;
-		} while (input != 'z' && input != 'x');
-		switch(input){
-		case 'z': {
-			data.advance(false);
-			break;
+		while(window.pollEvent(event) != sf::Event::Closed)
+		{
+			if(keyboard.isKeyPressed((sf::Keyboard::Z)){
+				data.advance(false);
+				break;
+			}
+			else if(keyboard.isKeyPressed((sf::Keyboard::X){
+				data.advance(true);
+				break;	
+			}
 		}
-		case 'x': {
-			data.advance(true);
-			break;
-		}
+		if(window.pollEvent(event) == sf::Event::Closed)
+		{
+			break;					      
 		}
 	}
 
